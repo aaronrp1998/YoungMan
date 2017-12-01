b5dfbd28f6434082ad4f8b2c524343352828226c
Aaron <aaronrp1998@gmail.com>
2017-12-01 14:44:50 +0100

    cambios3

--- proyecto/src/js/bundle.js
+++ proyecto/src/js/bundle.js
@@ -4,7 +4,7 @@ function preload() {
     game.load.baseURL = 'https://aaronrp1998.github.io/YoungMan/proyecto/src/'
     game.load.crossOrigin = 'anonymus';
     game.load.image('background', 'images/background.png');
-    game.load.image('mainCharacter', 'images/megaman.png');
+    game.load.image('mainCharacter', 'images/Megaman.png');
     game.load.image('bala', 'images/bala.png');
 
 }
@@ -25,7 +25,7 @@ function create() {
 
 function update () {
 
-  
+
 
     if (game.input.keyboard.isDown(39))
     {
@@ -38,7 +38,7 @@ function update () {
     else if (game.input.keyboard.isDown(87))
     {
         bala = game.add.sprite(mainCharacter.x-60 , mainCharacter.y+4, 'bala');
-    } 
+    }
     else
     {
       mainCharacter.body.velocity.set(0);
