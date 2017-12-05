553dfb702316d3723d4eb225fd54bfb40ba4e072
Aaron <aaronrp1998@gmail.com>
2017-12-05 12:27:48 +0100

    intentando arreglar errores

--- proyecto/src/js/boot.js
+++ proyecto/src/js/boot.js
@@ -4,19 +4,19 @@ Boot = {
   preload: function() {
     clicBoton: '',
     YoungMan.load.image('pantallaInicialFondo', 'images/pantallaInicialFondo.jpg');
-    YoungMan.load.spritesheet('pantallaInicialBotones', 'sprites/pantallaInicialBotones.png', 201, 71.666666666);
-    YoungMan.load.audio('pantallaInicialMusica', 'audio/pantallaInicialMusica');
-    YoungMan.load.audio('pantallaInicialClic', 'audio/pantallaInicialClic');
+    YoungMan.load.image('pantallaInicialBotones', 'sprites/pantallaInicialBotones.png');
+    YoungMan.load.audio('pantallaInicialMusica', 'audio/pantallaInicialMusica.mp3');
+    YoungMan.load.audio('pantallaInicialClic', 'audio/pantallaInicialClic.mp3');
   },
   create: function() {
     YoungMan.add.sprite(0, 0, 'pantallaInicialFondo');
     var musica = YoungMan.add.audio('pantallaInicialMusica');
     musica.play('', 0, 0.5, true);
     this.clicBoton = YoungMan.add.audio('pantallaInicialClic');
-    YoungMan.add.button(960, 540, 'pantallaInicialBotones', this.iniciar, this, 2, 1, 0);
+    YoungMan.add.button(960, 540, 'pantallaInicialBotones', this.iniciar, this);
   },
   iniciar: function(){
-    clicBoton.play();
+    this.clicBoton.play();
     console.log('Inicia videojuego');
   }
 }
