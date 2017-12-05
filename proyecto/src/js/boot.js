feee1260c9fbf28eea223fc7c27a894848d49c1d
Aaron <aaronrp1998@gmail.com>
2017-12-05 12:28:43 +0100

    error

--- proyecto/src/js/boot.js
+++ proyecto/src/js/boot.js
@@ -13,7 +13,7 @@ Boot = {
     var musica = YoungMan.add.audio('pantallaInicialMusica');
     musica.play('', 0, 0.5, true);
     this.clicBoton = YoungMan.add.audio('pantallaInicialClic');
-    YoungMan.add.button(960, 540, 'pantallaInicialBotones', this.iniciar, this);
+    YoungMan.add.button(960, 540, 'pantallaInicialBotones', this.iniciar, this, 2, 0, 1);
   },
   iniciar: function(){
     this.clicBoton.play();
