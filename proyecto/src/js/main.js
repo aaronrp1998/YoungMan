4b2afb77f6f3da476a1928daa19daecfbe732b6e
Aaron <aaronrp1998@gmail.com>
2017-12-16 12:24:22 +0100

    c

--- proyecto/src/js/main.js
+++ proyecto/src/js/main.js
@@ -4,23 +4,21 @@ Main = {
   function preload() {
     YoungMan.load.image('background', 'images/background.jpg');
     YoungMan.load.image('mainCharacter', 'images/Megaman.png');
-    YoungMan.load.tilemap('tilemap', 'images/tilemap.json', null, Phaser.Tilemap.TILED_JSOM);
-    YoungMan.load.image('patron1Tilemap', 'images/52547184-muro-de-piedra-textura-de-fondo-Foto-de-archivo');
-    YoungMan.load.image('patron2Tilemap', 'images/New Piskel.png')
+    //YoungMan.load.tilemap('tilemap', 'images/tilemap.json', null, Phaser.Tilemap.TILED_JSOM);
+    //YoungMan.load.image('patron1Tilemap', 'images/52547184-muro-de-piedra-textura-de-fondo-Foto-de-archivo');
+    //YoungMan.load.image('patron2Tilemap', 'images/New Piskel.png')
   }
 
   function create() {
 
-    //  To make the sprite move we need to enable Arcade Physics
     YoungMan.physics.startSystem(Phaser.Physics.ARCADE);
+    YoungMan.physics.arcade.enable(mainCharacter);
 
     background = YoungMan.add.sprite(0, -10, 'background');
     mainCharacter = YoungMan.add.sprite(YoungMan.world.centerX, 410, 'mainCharacter');
-    map = YoungMan.add.tilemap('tilemap');
-    map.addTilesetImage("patron1", "patron1Tilemap");
-    map.addTilesetImage("patron2", "patron2Tilemap");
-    //  And enable the Sprite to have a physics body:
-    YoungMan.physics.arcade.enable(mainCharacter);
+    //map = YoungMan.add.tilemap('tilemap');
+    //map.addTilesetImage("patron1", "patron1Tilemap");
+    //map.addTilesetImage("patron2", "patron2Tilemap");
 
   }
 
@@ -37,9 +35,4 @@ Main = {
 
   }
 
-  function render () {
-
-	  YoungMan.debug.inputInfo(32, 32);
-
-  }
 }
