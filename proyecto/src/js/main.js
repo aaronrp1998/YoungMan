c51976640105d16d7672ed40a506576570253172
Aaron <aaronrp1998@gmail.com>
2017-12-16 12:12:51 +0100

    c

--- proyecto/src/js/main.js
+++ proyecto/src/js/main.js
@@ -1,31 +1,32 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var Main = Main || {};
 
-function preload() {
+Main = {
+  function preload() {
     game.load.baseURL = 'https://aaronrp1998.github.io/YoungMan/proyecto/src/'
     game.load.crossOrigin = 'anonymus';
-    game.load.image('background', 'images/background.png');
+    game.load.image('background', 'images/background.jpg');
     game.load.image('mainCharacter', 'images/Megaman.png');
-    game.load.image('bala', 'images/bala.png');
+    game.load.tilemap('tilemap', 'images/tilemap.json', null, Phaser.Tilemap.TILED_JSOM);
+    game.load.image('patron1Tilemap', 'images/52547184-muro-de-piedra-textura-de-fondo-Foto-de-archivo');
+    game.load.image('patron2Tilemap', 'images/New Piskel.png')
+  }
 
-}
-
-var sprite;
-
-function create() {
+  function create() {
 
     //  To make the sprite move we need to enable Arcade Physics
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     background = game.add.sprite(0, -10, 'background');
     mainCharacter = game.add.sprite(game.world.centerX, 410, 'mainCharacter');
+    map = game.add.tilemap('tilemap');
+    map.addTilesetImage("patron1", "patron1Tilemap");
+    map.addTilesetImage("patron2", "patron2Tilemap");
     //  And enable the Sprite to have a physics body:
     game.physics.arcade.enable(mainCharacter);
 
-}
+  }
 
-function update () {
-
-
+  function update () {
 
     if (game.input.keyboard.isDown(39))
     {
@@ -35,23 +36,12 @@ function update () {
     {
         game.physics.arcade.moveToXY(mainCharacter, 30, mainCharacter.position.y, 200);
     }
-    else if (game.input.keyboard.isDown(87))
-    {
-        bala = game.add.sprite(mainCharacter.x-60 , mainCharacter.y+4, 'bala');
-    }
-    else if (game.input.keyboard.isDown(32))
-    {
-      game.physics.arcade.moveToXY(mainCharacter, mainCharacter.x, mainCharacter.position.y - 200, 200);
-    }
-    else
-    {
-      mainCharacter.body.velocity.set(0);
-    }
 
-}
+  }
 
-function render () {
+  function render () {
 
-	game.debug.inputInfo(32, 32);
+	  game.debug.inputInfo(32, 32);
 
+  }
 }
